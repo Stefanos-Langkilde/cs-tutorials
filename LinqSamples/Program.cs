b010c690bbc2c21a5f0a2aa3e47ed716af354287
Stefanos-Langkilde <83694853+Stefanos-Langkilde@users.noreply.github.com>
2022-07-31 22:29:03 +0200

    Add files via upload

---
+++ LinqSamples/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using Features.Linq;
+
+namespace Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var developers = new Employee[]
+            {
+                new Employee { Id = 1, Name = "Scott" },
+                new Employee { Id = 2, Name = "Chris" }
+            };
+
+            var sales = new List<Employee>()
+            {
+                new Employee { Id = 3, Name = "Alex" }
+            };
+
+            var query = developers.Where(e => e.Name.Length == 5)
+                .OrderBy(e => e.Name);
+
+            foreach (var employee in query)
+            {
+                Console.WriteLine(employee.Name);
+            }
+            static bool NameStartsWithS(Employee employee)
+            {
+                return employee.Name.StartsWith("S");
+            }
+        }
+    }
+}
+
